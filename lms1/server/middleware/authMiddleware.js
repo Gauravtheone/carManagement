241158da8c0a4abffac4ef7014a5fb1f2af1fb52
Gauravtheone <121490722+Gauravtheone@users.noreply.github.com>
2025-02-05 22:32:45 +0530

    Update authMiddleware.js

--- lms1/server/middleware/authMiddleware.js
+++ lms1/server/middleware/authMiddleware.js
@@ -3,8 +3,9 @@ const jwt = require("jsonwebtoken");
 const authenticate = (req, res, next) => {
   try {
     const token = req.cookies?.token; // Ensure req.cookies exists
+    console.log("ye Lo" + token)
     if (!token) return res.status(401).json({ message: "Unauthorized" });
-
+console.log(token)
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // Attach user info to request
     next();
